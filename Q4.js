732ce82f9212611af2203a7d4d4d6abafaa0791b
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:35:39 +0500

    NODEJS AssignmentQ4
    4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:

---
+++ Q4.js
@@ -0,0 +1,3 @@
+"use strict";
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+console.log("“When you have a dream, you've got to grab it and never let go.”");
